e026abfdf954ab6db759e4fd243be51239e65644
Vishal <vishalbhola5@gmail.com>
2019-12-22 20:31:31 +0530

    User and UserItem Structured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component{
@@ -8,7 +8,10 @@ class App extends Component{
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github"/>
-        <UserItem />
+        <div className="container">
+          <Users /> 
+        </div>
+        
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react'
 
 export class UserItem extends Component {
-
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    };
-
     render() {
 
-        const { login, avatar_url, html_url } = this.state;
+        const { login, avatar_url, html_url } = this.props.user;
 
         return (
             <div className="card text-center">
