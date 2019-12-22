d3630c8f45e5bc078220fe7de3de65fa9eb5b074
Vishal <vishalbhola5@gmail.com>
2019-12-22 19:33:20 +0530

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        <Navbar title="Github Finder" icon="fab fa-github"/>
       </div>
     );
   }
-}
+} 
 
 export default App;
