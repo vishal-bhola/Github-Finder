5129158b2fa44b329eabf5086cfa5cdcb9992076
Vishal <vishalbhola5@gmail.com>
2019-12-27 15:47:44 +0530

    User Class to Function with useEffect Hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,41 +1,31 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types'
 import Repos from '../repos/Repos';
 import {Link} from 'react-router-dom';
 
-export class User extends Component {
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 
-    componentDidMount(){
-        this.props.getUser(this.props.match.params.login); 
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+    useEffect(()=>{
+        getUser(match.params.login); 
+        getUserRepos(match.params.login);
+        //eslint-disable-next-line
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser :PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-    }
-
-    render() {
-        const { 
-            name,
-            avatar_url,
-            location,
-            bio,
-            blog,
-            login,
-            html_url,
-            company,
-            followers,
-            following,
-            public_repos,
-            public_gists,
-            hireable } = this.props.user;
-
-        const { loading,repos } = this.props;
+    const { 
+        name,
+        avatar_url,
+        location,
+        bio,
+        blog,
+        login,
+        html_url,
+        company,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable } = user;
 
         if(loading)
         return <Spinner />
@@ -95,7 +85,14 @@ export class User extends Component {
 
             </Fragment>
         )
-    }
 }
 
-export default User
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser :PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+}
+
+export default User;
