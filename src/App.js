f2b5b68de5b3cf4c3665e0462088483edb842ae8
Vishal <vishalbhola5@gmail.com>
2019-12-27 14:52:01 +0530

    Repos & RepoItem Component & Data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component{
   state = {
     users:[],
     user: {},
+    repos:[],
     loading: false,
     alert:null
   }
@@ -39,6 +40,14 @@ class App extends Component{
 
     const res  = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ user: res.data , loading: false });
+  }
+
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({loading: true});
+
+    const res  = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ repos: res.data , loading: false });
   }
 
   // Clear users from state
@@ -75,7 +84,7 @@ class App extends Component{
               <Route exact path='/about' component={About} />
 
               <Route exact path="/user/:login" render={props =>(
-                <User {...props} getUser={this.getUser} user={this.state.user} loading={this.state.loading}/>
+                <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} user={this.state.user} repos={this.state.repos} loading={this.state.loading} />
               )} />
 
             </Switch>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,18 +1,22 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types'
+import Repos from '../repos/Repos';
 import {Link} from 'react-router-dom';
 
 export class User extends Component {
 
     componentDidMount(){
         this.props.getUser(this.props.match.params.login); 
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
         getUser :PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
     }
 
     render() {
@@ -31,7 +35,7 @@ export class User extends Component {
             public_gists,
             hireable } = this.props.user;
 
-        const { loading } = this.props;
+        const { loading,repos } = this.props;
 
         if(loading)
         return <Spinner />
@@ -86,6 +90,9 @@ export class User extends Component {
                     <div className="badge badge-light">Public Repos: {public_repos}</div>
                     <div className="badge badge-dark">Public Gists: {public_gists}</div>
                 </div>
+
+                <Repos repos={repos} />
+
             </Fragment>
         )
     }
